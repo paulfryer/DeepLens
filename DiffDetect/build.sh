c337712d9a546775c8e31326590a2e888b2ef01c
Paul Fryer <paulfryer@macrowebservices.com>
2018-07-25 09:01:11 -0700

    Create build.sh

---
+++ DiffDetect/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing required modules."
+sudo pip install -r requirements.txt -t .
+echo "Removing test folders to lower overall size of Lambda deployment."
+sudo find . -name "tests" -type d -exec rm -r "{}" \;
